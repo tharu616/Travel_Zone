5570aa5809bcd9e3b16f861ed0a13a4e7cbc6b66
tharu616 <tharusharukshan573@gmail.com>
2026-06-17 14:27:59 +0530

    Initial commit

---
+++ Travelzone_backend/src/main/java/com/travelzone/TravelzoneApplication.java
@@ -0,0 +1,11 @@
+package com.travelzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelzoneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelzoneApplication.class, args);
+    }
+}
---
+++ Travelzone_backend/src/test/java/com/travelzone/TravelzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
